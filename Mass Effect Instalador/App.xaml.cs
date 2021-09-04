55d26a354a6f9a77a80c0c62b6fba083f5d57dcb
klooke <klookegames@hotmail.com>
2021-09-04 20:40:27 -0300

    Refatora completo
    - Remove linhas não utilizadas e underline do namespace.
    - Modifica nome de variáveis, métodos e outros para fácil identificação.
    - Cria resources para colocar arquivos de texto utilizadas no processo.
    - Modifica mensagens de alguns erros para fácil entendimento.
    - Encapsula métodos para verificação de diretórios de instalação e arquivos.

--- Mass Effect Instalador/App.xaml.cs
+++ Mass Effect Instalador/App.xaml.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Mass_Effect_Instalador
+namespace MassEffectInstalador
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-        public static string cmd = string.Empty;
+        public static string directoryGame = string.Empty;
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            MainWindow wnd = new MainWindow();
-            cmd = e.Args[0];
+            MainWindow wnd = new();
+            directoryGame = e.Args[0];
             wnd.Show();
         }
     }
--- Mass Effect Instalador/MainWindow.xaml.cs
+++ Mass Effect Instalador/MainWindow.xaml.cs
@@ -5,14 +5,15 @@ using System.ComponentModel;
 using System.IO;
 using System.Windows;
 
-namespace Mass_Effect_Instalador
+namespace MassEffectInstalador
 {
     public partial class MainWindow : Window
     {
-        const int LE1_FILES_TLK = 1678;
+        private const int LE1_TLK_COUNT = 1678;
+        private const int LE1_PCC_COUNT = 406;
 
         private bool isBusy;
-        private bool checkFiles;
+        private bool packagesExists;
         private int countFiles;
 
         public MainWindow()
@@ -22,8 +23,8 @@ namespace Mass_Effect_Instalador
             PackageSaver.Initialize();
         }
 
-        private void MainWindow_Loaded(Object sender, EventArgs e)
-        { 
+        private void MainWindow_Loaded(object sender, EventArgs e)
+        {
             Rect desktopWorkingArea = SystemParameters.WorkArea;
             Left = desktopWorkingArea.Right - Width;
             Top = desktopWorkingArea.Bottom - Height;
@@ -35,106 +36,109 @@ namespace Mass_Effect_Instalador
                 WorkerReportsProgress = true
             };
             worker.DoWork += Worker_DoWork;
-            worker.ProgressChanged += worker_ProgressChanged;
-            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
             worker.RunWorkerAsync(10000);
         }
         private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
-            if (isBusy)
+            if(isBusy)
             {
-                if (MessageBox.Show(this, "A instalação ainda não está concluida, se fechar agora todos os arquivos podem ser corrompidos, tem certeza que deseja sair?", "Atenção", MessageBoxButton.YesNo, MessageBoxImage.Stop) == MessageBoxResult.No)
-                {
+                if(MessageBox.Show(this, "A instalação ainda não está concluida, se fechar agora alguns arquivos podem ser corrompidos, tem certeza que deseja sair?", "Atenção", MessageBoxButton.YesNo, MessageBoxImage.Stop) == MessageBoxResult.No)
                     e.Cancel = true;
-                }
             }
         }
 
         private void Worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            string directoryGame = App.cmd;
-            string directoryInstall = directoryGame + "\\Game\\ME1\\BioGame\\CookedPCConsole\\";
-            //MessageBox.Show(directoryGame);
-            //MessageBox.Show(directoryInstall);
-            if (File.Exists(Directory.GetCurrentDirectory() + "\\list.txt"))
+            string path = App.directoryGame + "\\Game\\ME1\\BioGame\\CookedPCConsole\\";
+            string newTalkPath = Directory.GetCurrentDirectory() + "\\Files\\";
+            string[] packagesLE1 = Properties.Resources.ListPackageLE1.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+            if(!CheckTalkToInstall(newTalkPath)) return;
+            if(!(packagesExists = PackagesExists(path, packagesLE1))) return;
+            if(!MakeBackup(path, packagesLE1)) return;
+
+            for(int i = 0; i < packagesLE1.Length; i++)
             {
-                checkFiles = true;
-                countFiles = 0;
-                string[] filesList = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\list.txt");
-
-                // Verificar se todos os arquivos existem e fazer backup
-                for (int i = 0; i < filesList.Length; i++)
+                isBusy = true;
+                using IMEPackage pcc = MEPackageHandler.OpenLE1Package(path + packagesLE1[i]);
+                for(int j = 0; j < pcc.LocalTalkFiles.Count; j++)
                 {
-                    if (!File.Exists(directoryInstall + filesList[i]))
+                    ME1TalkFile talkFile = pcc.LocalTalkFiles[j];
+                    if(talkFile.Name is "tlk_M" or "GlobalTlk_tlk_M" or "tlk" or "GlobalTlk_tlk")
                     {
-                        checkFiles = false;
-                        break;
-                    }
-                    else
-                    {
-                        DirectoryInfo infoNewDir = Directory.CreateDirectory(directoryGame + "\\_Backup");
-                        if (!File.Exists(infoNewDir.FullName + "\\" + filesList[i])) File.Copy(directoryInstall + filesList[i], infoNewDir.FullName + "\\" + filesList[i]);
+                        string talkPath = newTalkPath + talkFile.BioTlkSetName + "." + talkFile.Name + ".xml";
+                        HuffmanCompression compressor = new();
+                        compressor.LoadInputData(talkPath);
+                        compressor.serializeTalkfileToExport(pcc.GetUExport(talkFile.UIndex), true);
+                        countFiles++;
                     }
                 }
-
-                for (int i = 0; i < filesList.Length; i++)
-                {
-                    if (!checkFiles) break;
-
-                    isBusy = true;
-                    using IMEPackage pcc = MEPackageHandler.OpenLE1Package(directoryInstall + filesList[i]);
-
-                    for (int j = 0; j < pcc.LocalTalkFiles.Count; j++)
-                    {
-                        var talkfile = pcc.LocalTalkFiles[j];
-                        if (talkfile.Name == "tlk_M" || talkfile.Name == "tlk" || talkfile.Name == "GlobalTlk_tlk" || talkfile.Name == "GlobalTlk_tlk_M")
-                        {
-                            var tlkPathName = Directory.GetCurrentDirectory() + "\\Files\\" + talkfile.BioTlkSetName + "." + talkfile.Name + ".xml";
-                            if (File.Exists(tlkPathName))
-                            {
-                                HuffmanCompression compressor = new();
-                                compressor.LoadInputData(tlkPathName);
-                                compressor.serializeTalkfileToExport(pcc.GetUExport(talkfile.UIndex), true);
-                                countFiles++;
-                            }
-                            else
-                            {
-                                checkFiles = false;
-                                break;
-                            }
-                        }
-                    }
-                    (sender as BackgroundWorker).ReportProgress(i);
-                }
+                (sender as BackgroundWorker).ReportProgress(i);
             }
         }
-        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             Dir.Value = e.ProgressPercentage;
             string s = (int)(e.ProgressPercentage / Dir.Maximum * 100) + "%";
             Title = "Extração: " + s;
             textTitulo.Text = "Extraindo... " + s;
         }
-        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             isBusy = false;
-            if (checkFiles)
+            if(packagesExists)
             {
-                if (countFiles == LE1_FILES_TLK)
-                {
+                if(countFiles == LE1_TLK_COUNT)
                     MessageBox.Show(this, "Instalação foi concluida com êxito, aproveite!", "Atenção", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
                 else
-                {
-                    MessageBox.Show(this, "Alguns arquivos não foram encontrados por isso a tradução pode está incompleta. Por favor desinstale a tradução, repare o jogo e tente novamente.", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
-                }
+                    MessageBox.Show(this, "Alguns arquivos não foram encontrados, por isso a tradução pode está incompleta.\n" +
+                        "Por favor reinstale a tradução.", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
             else
-            {
-                MessageBox.Show(this, "Não foi possivel concluir a instalação!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+                MessageBox.Show(this, "Não foi possivel concluir a instalação, arquivos não encontrados!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
 
             Application.Current.Shutdown();
+        }
+        private static bool CheckTalkToInstall(string path)
+        {
+            if (!Directory.Exists(path))
+                return false;
+            else if (Directory.GetFiles(path, "*.xml").Length != LE1_PCC_COUNT)
+                return false;
+
+            return true;
+        }
+        private static bool PackagesExists(string path, string[] files)
+        {
+            if(!Directory.Exists(path) || files.Length != LE1_PCC_COUNT)
+                return false;
+            
+            foreach(string file in files)
+            {
+                if(!File.Exists(path + file))
+                    return false;
+            }
+            return true;
+        }
+        private static bool MakeBackup(string path, string[] files)
+        {
+            try
+            {
+                DirectoryInfo backupDirectory = Directory.CreateDirectory(App.directoryGame + "\\_Backup");
+                foreach(string file in files)
+                {
+                    if(!File.Exists(backupDirectory.FullName + "\\" + file))
+                        File.Copy(path + file, backupDirectory.FullName + "\\" + file);
+                }
+                return true;
+            }
+            catch(IOException e)
+            {
+                MessageBox.Show("Não foi possivel fazer o backup, a instalação não pode ser concluida!\n\n" + e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
         }
     }
 }
